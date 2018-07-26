cc5d15b704c935b730d8b93ba8d26e0fa64ccb70
Matt Kirby <MattKir@aol.com>
2018-07-26 15:52:21 -0700

    Completed class demo

---
+++ app/add_to_dom.js
@@ -0,0 +1,8 @@
+
+export default function(elementType, text){
+    const newElement = document.createElement(elementType);
+    
+    newElement.innerText = text;
+
+    document.body.appendChild(newElement);
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import {Person, SuperPerson} from './person';
+
+const bob = new Person('Walter');
+
+console.log(bob.speak('I am the one who knocks!'));
+
+const superSarah = new SuperPerson('Sarah', 'Tip toe real quiet');
+
+superSarah.speak('Hi I\'m super Sarah');
+
+superSarah.saySuperpower();
